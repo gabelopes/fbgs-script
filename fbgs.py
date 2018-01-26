dde1b397e1685e60a0e6f49bf1eaad34cac6e4b7
Gabriel <gabriel.nunes@sap.com>
2018-01-26 17:32:53 -0200

    Initial skeleton

---
+++ fbgs.py
@@ -0,0 +1,25 @@
+import click
+import webbrowser
+import re
+
+Parameters = {
+    search = r""
+    like = r""
+    range = r""
+    name = r""
+}
+
+def search():
+
+
+def parametrize(argument, pattern):
+
+def parametrize_all(arguments, pattern):
+
+def parametrize_birth(year, month):
+
+def parametrize_range(range):
+    return re.sub()
+
+if __name__ == "__main__":
+    search()
